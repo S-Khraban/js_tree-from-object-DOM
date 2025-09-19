924a9f415fbdd5f1a46671bd4638bf76b5473529
S-Khraban <s.hraban@i.ua>
2025-09-19 20:13:41 +0300

    make build

---
+++ js_tree-from-object-DOM.f75de5e1.js
@@ -0,0 +1,37 @@
+'use strict';
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+function createTree(element, data) {
+    if (!element || !data || typeof data !== 'object') return;
+    const keys = Object.keys(data);
+    if (keys.length === 0) return;
+    const ul = document.createElement('ul');
+    for (const key of keys){
+        const li = document.createElement('li');
+        li.textContent = key;
+        const value = data[key];
+        if (value && typeof value === 'object' && Object.keys(value).length > 0) createTree(li, value);
+        ul.appendChild(li);
+    }
+    element.appendChild(ul);
+}
+if (typeof window !== 'undefined') {
+    window.createTree = createTree;
+    window.food = food;
+}
+
+//# sourceMappingURL=js_tree-from-object-DOM.f75de5e1.js.map
